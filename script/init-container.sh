7dade89de26111476ff403019278308f1cf6a6d6
JakeZT <jakeottawaca@gmail.com>
2024-04-02 00:06:32 -0400

    Fixed commands

--- script/init-container.sh
+++ script/init-container.sh
@@ -3,37 +3,52 @@
 # Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' | sudo tee -a ~/.bashrc
+# echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' | sudo tee -a ~/.bashrc
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/vscode/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 echo 'export PATH="/usr/local/bin:$PATH"' | sudo tee -a ~/.bashrc
-source ~/.bashrc
+echo "source ~/.bashrc" | source ~/.bashrc
+
+#Enter zsh
+brew install zsh powerlevel10k
 
 
 #Install dependencies
-brew install python pyenv pyenv-virtualenv poetry volta mysql postgresql  redis zsh powerlevel10k
+brew install python pyenv pyenv-virtualenv poetry mysql postgresql  redis
 
 # Install Bun
 curl -fsSL https://bun.sh/install | bash
-source /root/.bashrc
+source /home/vscode/.bashrc
+source ~/.bashrc
+
+
+
+#Install volta
+brew install volta
+
+# update bashrc and zshrc
+(
+echo 'export VOLTA_HOME="$HOME/.volta"'
+echo 'export PATH="$VOLTA_HOME/bin:$PATH"'
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
+echo 'export BUN_INSTALL="$HOME/.bun"'
+echo 'export PATH="$BUN_INSTALL/bin:$PATH"'
+) | tee -a ~/.bashrc ~/.zshrc >/dev/null
+
+(
+echo 'source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme'
+echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true'
+) >> ~/.zshrc
+
+
+source ~/.bashrc
+source ~/.zshrc
 
 #Install node
 volta install node@18
 
-#Enter zsh
-/bin/zsh
-
-#Update environment path
-(echo 'export VOLTA_HOME="$HOME/.volta"'
-echo 'export PATH="$VOLTA_HOME/bin:$PATH"'
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"'
-echo 'source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme'
-echo 'POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true'
-echo 'export BUN_INSTALL="$HOME/.bun"'
-echo 'export PATH="$BUN_INSTALL/bin:$PATH"'
-) >> ~/.zshrc
-
-source ~/.zshrc
-
 # End
 echo "Dependencies have been installed successfully!"
 
-# echo "p10k configure to config zsh theme"
+echo "p10k configure to config zsh theme"
+echo "/bin/zsh"
